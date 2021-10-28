3dd50219cf435deed2207f4e1515a30ea680cae3
DeryaGulmyradov <derya92turkmen@gmail.com>
2021-10-27 19:49:58 -0500

    feat: add counter field and increment button

--- src/components/employeeForm/Form.style.js
+++ src/components/employeeForm/Form.style.js
@@ -1,8 +1,15 @@
 import { makeStyles } from '@material-ui/core/styles';
 
 export const useStyles = makeStyles({
+  formWrapper: {
+    paddingTop: '60px',
+  },
   textField: {
     maxWidth: 600,
-    lineHeight: 24,
+  },
+  root: {
+    marginBottom: 24,
+    minWidth: 600,
+    display: 'flex',
   },
 });
--- src/components/employeeForm/FormGrid.js
+++ src/components/employeeForm/FormGrid.js
@@ -1,36 +1,53 @@
 import React from 'react';
-import { Grid } from '@material-ui/core';
+import { Button, Grid } from '@material-ui/core';
 import { TextField } from 'mui-rff';
 import { useStyles } from './Form.style';
 import { SubmitButton } from './SubmitButton';
 
-export const FormGrid = () => {
-  const { textField } = useStyles();
+export const FormGrid = ({ onIncrement }) => {
+  const { root, textField, counterField } = useStyles();
 
   return (
     <>
       <Grid container item direction='column' alignItems='center' spacing={2}>
-        <TextField
-          label='First Name'
-          name='firstName'
-          placeholder='First Name'
-          className={textField}
-          required
-        />
-        <TextField
-          label='Last Name'
-          name='lastName'
-          placeholder='Last Name'
-          className={textField}
-          required
-        />
-        <TextField
-          label='Desired Hourly Rate'
-          name='hourlyRate'
-          placeholder='Desired Hourly Rate'
-          className={textField}
-          required
-        />
+        <div className={root}>
+          <TextField
+            label='First Name'
+            name='firstName'
+            placeholder='First Name'
+            className={textField}
+            required
+          />
+        </div>
+        <div className={root}>
+          <TextField
+            label='Last Name'
+            name='lastName'
+            placeholder='Last Name'
+            className={textField}
+            required
+          />
+        </div>
+        <div className={root}>
+          <TextField
+            label='Desired Hourly Rate'
+            name='hourlyRate'
+            placeholder='Desired Hourly Rate'
+            className={textField}
+            required
+          />
+        </div>
+        <div className={root}>
+          <TextField
+            label='Counter Value'
+            name='counter'
+            placeholder='Counter Value'
+            disabled={true}
+          />
+          <Button color='primary' variant='contained' onClick={onIncrement}>
+            +
+          </Button>
+        </div>
       </Grid>
 
       {SubmitButton}
